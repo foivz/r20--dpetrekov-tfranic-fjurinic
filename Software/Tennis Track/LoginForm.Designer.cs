1740703760dbfce941cb17232a066eb9e7173f5d
Domagoj Petreković <dpetrekov@foi.hr>
2020-04-03 20:57:34 +0200

    LoginScreen

---
+++ Software/Tennis Track/LoginForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Tennis_Track
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.BtnPrijaviSe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtUsername.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.TxtUsername.Location = new System.Drawing.Point(275, 161);
+            this.TxtUsername.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.TxtUsername.Multiline = true;
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(250, 30);
+            this.TxtUsername.TabIndex = 0;
+            this.TxtUsername.Text = "Korisničko ime";
+            this.TxtUsername.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TxtUsername_MouseDoubleClick);
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPassword.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.TxtPassword.Location = new System.Drawing.Point(275, 204);
+            this.TxtPassword.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.TxtPassword.Multiline = true;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(250, 30);
+            this.TxtPassword.TabIndex = 1;
+            this.TxtPassword.Text = "Lozinka";
+            this.TxtPassword.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TxtPassword_MouseDoubleClick);
+            // 
+            // BtnPrijaviSe
+            // 
+            this.BtnPrijaviSe.Location = new System.Drawing.Point(324, 247);
+            this.BtnPrijaviSe.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.BtnPrijaviSe.Name = "BtnPrijaviSe";
+            this.BtnPrijaviSe.Size = new System.Drawing.Size(153, 43);
+            this.BtnPrijaviSe.TabIndex = 2;
+            this.BtnPrijaviSe.Text = "Prijavi se";
+            this.BtnPrijaviSe.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnPrijaviSe);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUsername);
+            this.Name = "LoginForm";
+            this.Text = "Tennis Track";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button BtnPrijaviSe;
+    }
+}
+
---
+++ Software/Tennis Track/LoginForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tennis_Track
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtUsername_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            TxtUsername.SelectAll();
+        }
+
+        private void TxtPassword_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            TxtPassword.SelectAll();
+        }
+    }
+}
