72cd8e675ea939a12447a147b0f34c9ac4c18205
Domagoj Petreković <dpetrekov@foi.hr>
2020-06-16 22:50:35 +0200

    Sitne promjene brisanju meceva

--- Software/Tennis Track/Forme/Brisanje mečeva.Designer.cs
+++ Software/Tennis Track/Forme/Brisanje mečeva.Designer.cs
@@ -31,25 +31,25 @@
             this.components = new System.ComponentModel.Container();
             this.btnPovratak = new System.Windows.Forms.Button();
             this.mecDataGridView = new System.Windows.Forms.DataGridView();
-            this.btnBrisiMec = new System.Windows.Forms.Button();
-            this.mecBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Igrac_1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ImeDrugiClan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnBrisiMec = new System.Windows.Forms.Button();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mecBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.mecDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mecBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPovratak
             // 
-            this.btnPovratak.Location = new System.Drawing.Point(651, 401);
+            this.btnPovratak.Location = new System.Drawing.Point(1630, 787);
             this.btnPovratak.Margin = new System.Windows.Forms.Padding(15);
             this.btnPovratak.Name = "btnPovratak";
-            this.btnPovratak.Size = new System.Drawing.Size(125, 25);
+            this.btnPovratak.Size = new System.Drawing.Size(250, 50);
             this.btnPovratak.TabIndex = 8;
             this.btnPovratak.Text = "Povratak";
             this.btnPovratak.UseVisualStyleBackColor = true;
@@ -57,6 +57,8 @@
             // 
             // mecDataGridView
             // 
+            this.mecDataGridView.AllowUserToAddRows = false;
+            this.mecDataGridView.AllowUserToDeleteRows = false;
             this.mecDataGridView.AutoGenerateColumns = false;
             this.mecDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.mecDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -68,40 +70,19 @@
             this.dataGridViewTextBoxColumn6,
             this.dataGridViewTextBoxColumn7});
             this.mecDataGridView.DataSource = this.mecBindingSource;
-            this.mecDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.mecDataGridView.Location = new System.Drawing.Point(24, 12);
+            this.mecDataGridView.Margin = new System.Windows.Forms.Padding(15, 3, 15, 3);
             this.mecDataGridView.Name = "mecDataGridView";
-            this.mecDataGridView.Size = new System.Drawing.Size(800, 182);
+            this.mecDataGridView.ReadOnly = true;
+            this.mecDataGridView.Size = new System.Drawing.Size(1856, 300);
             this.mecDataGridView.TabIndex = 9;
-            // 
-            // btnBrisiMec
-            // 
-            this.btnBrisiMec.Location = new System.Drawing.Point(651, 346);
-            this.btnBrisiMec.Margin = new System.Windows.Forms.Padding(15);
-            this.btnBrisiMec.Name = "btnBrisiMec";
-            this.btnBrisiMec.Size = new System.Drawing.Size(125, 25);
-            this.btnBrisiMec.TabIndex = 10;
-            this.btnBrisiMec.Text = "Obrisi meč";
-            this.btnBrisiMec.UseVisualStyleBackColor = true;
-            this.btnBrisiMec.Click += new System.EventHandler(this.btnBrisiMec_Click);
-            // 
-            // mecBindingSource
-            // 
-            this.mecBindingSource.DataSource = typeof(Tennis_Track.Baza_podataka.Mec);
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
-            this.dataGridViewTextBoxColumn1.HeaderText = "šifra meča";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewTextBoxColumn1.Width = 50;
             // 
             // Igrac_1
             // 
             this.Igrac_1.DataPropertyName = "ImePrviClan";
             this.Igrac_1.HeaderText = "Igrač 1";
             this.Igrac_1.Name = "Igrac_1";
+            this.Igrac_1.ReadOnly = true;
             this.Igrac_1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.Igrac_1.Width = 150;
             // 
@@ -110,32 +91,62 @@
             this.ImeDrugiClan.DataPropertyName = "ImeDrugiClan";
             this.ImeDrugiClan.HeaderText = "Igrač 2";
             this.ImeDrugiClan.Name = "ImeDrugiClan";
+            this.ImeDrugiClan.ReadOnly = true;
             this.ImeDrugiClan.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.ImeDrugiClan.Width = 150;
+            // 
+            // btnBrisiMec
+            // 
+            this.btnBrisiMec.Location = new System.Drawing.Point(1630, 707);
+            this.btnBrisiMec.Margin = new System.Windows.Forms.Padding(15);
+            this.btnBrisiMec.Name = "btnBrisiMec";
+            this.btnBrisiMec.Size = new System.Drawing.Size(250, 50);
+            this.btnBrisiMec.TabIndex = 10;
+            this.btnBrisiMec.Text = "Obriši meč";
+            this.btnBrisiMec.UseVisualStyleBackColor = true;
+            this.btnBrisiMec.Click += new System.EventHandler(this.btnBrisiMec_Click);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "šifra meča";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTextBoxColumn1.Width = 50;
             // 
             // dataGridViewTextBoxColumn11
             // 
             this.dataGridViewTextBoxColumn11.DataPropertyName = "ImeTurnira";
             this.dataGridViewTextBoxColumn11.HeaderText = "Turnir";
             this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn12
             // 
             this.dataGridViewTextBoxColumn12.DataPropertyName = "Rezultat";
             this.dataGridViewTextBoxColumn12.HeaderText = "Rezultat";
             this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            this.dataGridViewTextBoxColumn12.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn6
             // 
             this.dataGridViewTextBoxColumn6.DataPropertyName = "Datum";
             this.dataGridViewTextBoxColumn6.HeaderText = "Datum";
             this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn7
             // 
             this.dataGridViewTextBoxColumn7.DataPropertyName = "Vrijeme";
             this.dataGridViewTextBoxColumn7.HeaderText = "Vrijeme";
             this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            // 
+            // mecBindingSource
+            // 
+            this.mecBindingSource.DataSource = typeof(Tennis_Track.Baza_podataka.Mec);
             // 
             // Brisanje_mečeva
             // 
@@ -143,10 +154,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Tennis_Track.Properties.Resources.How_to_Grip_a_Tennis_Racket_Properly;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1904, 861);
             this.ControlBox = false;
-            this.Controls.Add(this.btnBrisiMec);
             this.Controls.Add(this.mecDataGridView);
+            this.Controls.Add(this.btnBrisiMec);
             this.Controls.Add(this.btnPovratak);
             this.Name = "Brisanje_mečeva";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Software/Tennis Track/Forme/Brisanje mečeva.cs
+++ Software/Tennis Track/Forme/Brisanje mečeva.cs
@@ -30,10 +30,9 @@ namespace Tennis_Track.Forme
 
         private void Brisanje_mečeva_Load(object sender, EventArgs e)
         {
-            tennisTrackEntities.Mec.Load();
-            mecBindingSource.DataSource = tennisTrackEntities.Mec.Local;
             mecDataGridView.AllowUserToAddRows = false;
             mecDataGridView.RowHeadersVisible = false;
+            OsvjeziMeceve();
         }
 
         private void btnPovratak_Click(object sender, EventArgs e)
@@ -48,6 +47,35 @@ namespace Tennis_Track.Forme
             mecBindingSource.Remove(mecZaObrisati);
             tennisTrackEntities.Mec.Remove(mecZaObrisati);
             tennisTrackEntities.SaveChanges();
+        }
+
+        private void OsvjeziMeceve()
+        {
+            int ukupnaSirinaStupaca = 0;
+            tennisTrackEntities.Mec.Load();
+            mecBindingSource.DataSource = tennisTrackEntities.Mec.Local;
+            for (int i = 0; i < mecDataGridView.Columns.Count; i++)
+            {
+                mecDataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                ukupnaSirinaStupaca = ukupnaSirinaStupaca + mecDataGridView.Columns[i].Width;
+            }
+
+            int razlikaSirine = mecDataGridView.Width - ukupnaSirinaStupaca;
+
+            //datagrid has calculated it's widths so we can store them
+            for (int i = 0; i <= mecDataGridView.Columns.Count - 1; i++)
+            {
+                mecDataGridView.Columns[i].DefaultCellStyle.Font = new Font("Arial", 18F, GraphicsUnit.Pixel);
+                //store autosized widths
+                int colw = mecDataGridView.Columns[i].Width + razlikaSirine/8;
+                //remove autosizing
+                mecDataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                //set width to calculated by autosize
+                mecDataGridView.Columns[i].Width = colw;
+
+                mecDataGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+            mecDataGridView.Columns[6].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
         }
     }
 }
