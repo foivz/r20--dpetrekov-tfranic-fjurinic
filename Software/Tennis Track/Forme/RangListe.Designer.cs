7e2078463ff5641ba1cc2144b91064f822416d66
Tin Franić <tfranic@foi.hr>
2020-06-18 21:12:11 +0200

    Dodan forma RangListe

--- Software/Tennis Track/Forme/RangListe.Designer.cs
+++ Software/Tennis Track/Forme/RangListe.Designer.cs
@@ -28,40 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dgvRangLista = new System.Windows.Forms.DataGridView();
-            this.txtPrezimeIliImeKorisnika = new System.Windows.Forms.TextBox();
-            this.btnFiltriraj = new System.Windows.Forms.Button();
             this.btnPovratak = new System.Windows.Forms.Button();
-            this.btnPrikaziSve = new System.Windows.Forms.Button();
+            this.BrojBodova = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SluzbenePobjede = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UkupnoPobjeda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gboRangiranje = new System.Windows.Forms.GroupBox();
+            this.rbtnBodovi = new System.Windows.Forms.RadioButton();
+            this.rbtnPobjede = new System.Windows.Forms.RadioButton();
+            this.imeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prezimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.korisnickoImeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefonDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clanBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgvRangLista)).BeginInit();
+            this.gboRangiranje.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.clanBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvRangLista
             // 
+            this.dgvRangLista.AutoGenerateColumns = false;
             this.dgvRangLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRangLista.Location = new System.Drawing.Point(44, 56);
+            this.dgvRangLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.imeDataGridViewTextBoxColumn,
+            this.prezimeDataGridViewTextBoxColumn,
+            this.BrojBodova,
+            this.SluzbenePobjede,
+            this.UkupnoPobjeda,
+            this.korisnickoImeDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.telefonDataGridViewTextBoxColumn});
+            this.dgvRangLista.DataSource = this.clanBindingSource;
+            this.dgvRangLista.Location = new System.Drawing.Point(12, 12);
             this.dgvRangLista.Name = "dgvRangLista";
-            this.dgvRangLista.Size = new System.Drawing.Size(712, 303);
+            this.dgvRangLista.Size = new System.Drawing.Size(776, 346);
             this.dgvRangLista.TabIndex = 3;
-            // 
-            // txtPrezimeIliImeKorisnika
-            // 
-            this.txtPrezimeIliImeKorisnika.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPrezimeIliImeKorisnika.Location = new System.Drawing.Point(44, 365);
-            this.txtPrezimeIliImeKorisnika.Multiline = true;
-            this.txtPrezimeIliImeKorisnika.Name = "txtPrezimeIliImeKorisnika";
-            this.txtPrezimeIliImeKorisnika.Size = new System.Drawing.Size(225, 30);
-            this.txtPrezimeIliImeKorisnika.TabIndex = 4;
-            this.txtPrezimeIliImeKorisnika.Text = "Unesite prezime ili ime";
-            // 
-            // btnFiltriraj
-            // 
-            this.btnFiltriraj.Location = new System.Drawing.Point(275, 364);
-            this.btnFiltriraj.Name = "btnFiltriraj";
-            this.btnFiltriraj.Size = new System.Drawing.Size(125, 30);
-            this.btnFiltriraj.TabIndex = 5;
-            this.btnFiltriraj.Text = "Filtriraj";
-            this.btnFiltriraj.UseVisualStyleBackColor = true;
             // 
             // btnPovratak
             // 
@@ -74,14 +78,95 @@
             this.btnPovratak.UseVisualStyleBackColor = true;
             this.btnPovratak.Click += new System.EventHandler(this.btnPovratak_Click);
             // 
-            // btnPrikaziSve
+            // BrojBodova
             // 
-            this.btnPrikaziSve.Location = new System.Drawing.Point(406, 365);
-            this.btnPrikaziSve.Name = "btnPrikaziSve";
-            this.btnPrikaziSve.Size = new System.Drawing.Size(125, 30);
-            this.btnPrikaziSve.TabIndex = 9;
-            this.btnPrikaziSve.Text = "Prikaži sve";
-            this.btnPrikaziSve.UseVisualStyleBackColor = true;
+            this.BrojBodova.DataPropertyName = "BrojBodova";
+            this.BrojBodova.HeaderText = "BrojBodova";
+            this.BrojBodova.Name = "BrojBodova";
+            // 
+            // SluzbenePobjede
+            // 
+            this.SluzbenePobjede.DataPropertyName = "SluzbenePobjede";
+            this.SluzbenePobjede.HeaderText = "SluzbenePobjede";
+            this.SluzbenePobjede.Name = "SluzbenePobjede";
+            // 
+            // UkupnoPobjeda
+            // 
+            this.UkupnoPobjeda.DataPropertyName = "UkupnoPobjeda";
+            this.UkupnoPobjeda.HeaderText = "UkupnoPobjeda";
+            this.UkupnoPobjeda.Name = "UkupnoPobjeda";
+            // 
+            // gboRangiranje
+            // 
+            this.gboRangiranje.Controls.Add(this.rbtnPobjede);
+            this.gboRangiranje.Controls.Add(this.rbtnBodovi);
+            this.gboRangiranje.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gboRangiranje.Location = new System.Drawing.Point(12, 364);
+            this.gboRangiranje.Name = "gboRangiranje";
+            this.gboRangiranje.Size = new System.Drawing.Size(282, 47);
+            this.gboRangiranje.TabIndex = 9;
+            this.gboRangiranje.TabStop = false;
+            this.gboRangiranje.Text = "Rangiraj po:";
+            // 
+            // rbtnBodovi
+            // 
+            this.rbtnBodovi.AutoSize = true;
+            this.rbtnBodovi.Checked = true;
+            this.rbtnBodovi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbtnBodovi.Location = new System.Drawing.Point(7, 22);
+            this.rbtnBodovi.Name = "rbtnBodovi";
+            this.rbtnBodovi.Size = new System.Drawing.Size(88, 19);
+            this.rbtnBodovi.TabIndex = 0;
+            this.rbtnBodovi.TabStop = true;
+            this.rbtnBodovi.Text = "Bodovima";
+            this.rbtnBodovi.UseVisualStyleBackColor = true;
+            this.rbtnBodovi.CheckedChanged += new System.EventHandler(this.rbtnBodovi_CheckedChanged);
+            // 
+            // rbtnPobjede
+            // 
+            this.rbtnPobjede.AutoSize = true;
+            this.rbtnPobjede.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rbtnPobjede.Location = new System.Drawing.Point(122, 22);
+            this.rbtnPobjede.Name = "rbtnPobjede";
+            this.rbtnPobjede.Size = new System.Drawing.Size(158, 19);
+            this.rbtnPobjede.TabIndex = 1;
+            this.rbtnPobjede.Text = "Ukupnim pobjedama";
+            this.rbtnPobjede.UseVisualStyleBackColor = true;
+            this.rbtnPobjede.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // imeDataGridViewTextBoxColumn
+            // 
+            this.imeDataGridViewTextBoxColumn.DataPropertyName = "Ime";
+            this.imeDataGridViewTextBoxColumn.HeaderText = "Ime";
+            this.imeDataGridViewTextBoxColumn.Name = "imeDataGridViewTextBoxColumn";
+            // 
+            // prezimeDataGridViewTextBoxColumn
+            // 
+            this.prezimeDataGridViewTextBoxColumn.DataPropertyName = "Prezime";
+            this.prezimeDataGridViewTextBoxColumn.HeaderText = "Prezime";
+            this.prezimeDataGridViewTextBoxColumn.Name = "prezimeDataGridViewTextBoxColumn";
+            // 
+            // korisnickoImeDataGridViewTextBoxColumn
+            // 
+            this.korisnickoImeDataGridViewTextBoxColumn.DataPropertyName = "KorisnickoIme";
+            this.korisnickoImeDataGridViewTextBoxColumn.HeaderText = "KorisnickoIme";
+            this.korisnickoImeDataGridViewTextBoxColumn.Name = "korisnickoImeDataGridViewTextBoxColumn";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // telefonDataGridViewTextBoxColumn
+            // 
+            this.telefonDataGridViewTextBoxColumn.DataPropertyName = "Telefon";
+            this.telefonDataGridViewTextBoxColumn.HeaderText = "Telefon";
+            this.telefonDataGridViewTextBoxColumn.Name = "telefonDataGridViewTextBoxColumn";
+            // 
+            // clanBindingSource
+            // 
+            this.clanBindingSource.DataSource = typeof(Tennis_Track.Baza_podataka.Clan);
             // 
             // RangListe
             // 
@@ -91,26 +176,35 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.btnPrikaziSve);
+            this.Controls.Add(this.gboRangiranje);
             this.Controls.Add(this.btnPovratak);
-            this.Controls.Add(this.btnFiltriraj);
-            this.Controls.Add(this.txtPrezimeIliImeKorisnika);
             this.Controls.Add(this.dgvRangLista);
             this.Name = "RangListe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rang liste";
             this.Load += new System.EventHandler(this.RangListe_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvRangLista)).EndInit();
+            this.gboRangiranje.ResumeLayout(false);
+            this.gboRangiranje.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.clanBindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView dgvRangLista;
-        private System.Windows.Forms.TextBox txtPrezimeIliImeKorisnika;
-        private System.Windows.Forms.Button btnFiltriraj;
         private System.Windows.Forms.Button btnPovratak;
-        private System.Windows.Forms.Button btnPrikaziSve;
+        private System.Windows.Forms.BindingSource clanBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prezimeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BrojBodova;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SluzbenePobjede;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UkupnoPobjeda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn korisnickoImeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefonDataGridViewTextBoxColumn;
+        private System.Windows.Forms.GroupBox gboRangiranje;
+        private System.Windows.Forms.RadioButton rbtnPobjede;
+        private System.Windows.Forms.RadioButton rbtnBodovi;
     }
 }
--- Software/Tennis Track/Forme/RangListe.cs
+++ Software/Tennis Track/Forme/RangListe.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tennis_Track.Baza_podataka;
+using Tennis_Track.Klase;
 
 namespace Tennis_Track.Forme
 {
     public partial class RangListe : Form
     {
+        TennisTrackEntities tennisTrackEntities = new TennisTrackEntities();
         private GlavniIzbornik glavniIzbornik;
 
         public RangListe()
@@ -25,15 +28,90 @@ namespace Tennis_Track.Forme
             InitializeComponent();
         }
 
+        private void RangListe_Load(object sender, EventArgs e)
+        {
+            DataGridViewColumn col = new DataGridViewTextBoxColumn();
+            col.HeaderText = "Rang";
+            dgvRangLista.Columns.Insert(0, col);
+            RangiranjePoBodovima();
+        }
         private void btnPovratak_Click(object sender, EventArgs e)
         {
             glavniIzbornik.Show();
             this.Close();
         }
 
-        private void RangListe_Load(object sender, EventArgs e)
+        public void RangiranjePoBodovima() 
         {
+                List<Clan> clanoviLista = (from c in tennisTrackEntities.Clan select c).ToList();
+                List<Clan> sortiranaLista = clanoviLista.OrderByDescending(c => c.BrojBodova).ToList();
+                clanBindingSource.DataSource = sortiranaLista.ToList();
 
+                
+
+                for (int i = 0; i < dgvRangLista.Rows.Count; i++)
+                {
+                    int rang = i + 1;
+                    dgvRangLista.Rows[i].Cells[0].Value = rang + ".".ToString();
+                }
+
+                for (int i = 0; i < dgvRangLista.Columns.Count; i++)
+                {
+                    dgvRangLista.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dgvRangLista.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                    dgvRangLista.Columns[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                }
+
+                dgvRangLista.AllowUserToAddRows = false;
+                dgvRangLista.RowHeadersVisible = false;
+
+                dgvRangLista.Columns[3].HeaderText = "Broj bodova";
+                dgvRangLista.Columns[4].HeaderText = "Sluzbenih pobjeda";
+                dgvRangLista.Columns[5].HeaderText = "Ukupno pobjeda";
+                dgvRangLista.Columns[6].HeaderText = "Korisnicko ime";
+
+                gboRangiranje.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void rbtnBodovi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnBodovi.Checked)
+            {
+                RangiranjePoBodovima();
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnPobjede.Checked)
+            {
+                List<Clan> clanoviLista = (from c in tennisTrackEntities.Clan select c).ToList();
+                List<Clan> sortiranaLista = clanoviLista.OrderByDescending(c => c.UkupnoPobjeda).ToList();
+                clanBindingSource.DataSource = sortiranaLista.ToList();
+
+                for (int i = 0; i < dgvRangLista.Rows.Count; i++)
+                {
+                    int rang = i + 1;
+                    dgvRangLista.Rows[i].Cells[0].Value = rang + ".".ToString();
+                }
+
+                for (int i = 0; i < dgvRangLista.Columns.Count; i++)
+                {
+                    dgvRangLista.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dgvRangLista.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                    dgvRangLista.Columns[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                }
+
+                dgvRangLista.AllowUserToAddRows = false;
+                dgvRangLista.RowHeadersVisible = false;
+
+                dgvRangLista.Columns[3].HeaderText = "Broj bodova";
+                dgvRangLista.Columns[4].HeaderText = "Sluzbenih pobjeda";
+                dgvRangLista.Columns[5].HeaderText = "Ukupno pobjeda";
+                dgvRangLista.Columns[6].HeaderText = "Korisnicko ime";
+
+                gboRangiranje.BackColor = System.Drawing.Color.Transparent;
+            }
         }
     }
 }
